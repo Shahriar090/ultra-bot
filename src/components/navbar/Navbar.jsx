b165861543ef139dd558a55ffc8d489d5cfc7620
Shahriar <shahriarh655@gmail.com>
2024-04-23 00:42:15 +0600

    Implementing navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,31 @@
 import "./navbar.css";
 const Navbar = () => {
   return (
-    <div>
-      <h1>navbar</h1>
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="logo">
+          <p>
+            Ultra <span>Bot</span>
+          </p>
+        </div>
+        <div className="navbar-links-container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is Ultra-Bot?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
