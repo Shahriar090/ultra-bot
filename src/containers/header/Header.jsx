702ec4e1593504f546b88b83b17792f4aae1a676
Shahriar <shahriarh655@gmail.com>
2024-04-25 14:10:24 +0600

    Implemented header section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,33 @@
 import "./header.css";
+import peopleImg from "../../assets/people.png";
+import aiImg from "../../assets/ai..png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header_section section__padding" id="home">
+      <div className="header_content">
+        <h1 className="gradient__text">
+          Let&apos;s Build Something Amazing With Ultra Bot
+        </h1>
+        <p>
+          Ultra Bot: Your AI Conversation Partner. Elevate your interactions
+          with our conversational AI. Engage, explore, and experience the future
+          of communication.
+        </p>
+        <div className="header_content_input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Start</button>
+        </div>
+        <div className="header_content_people">
+          <img src={peopleImg} alt="People Image" />
+          <p>1500+ People Requested Access to Visit in the Last 24 Hours</p>
+        </div>
+      </div>
+      <div className="header_image">
+        <img src={aiImg} alt="AI Image" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
