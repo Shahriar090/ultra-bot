c3376dde840f0cee8f7dec56bba58968447be5d2
Shahriar <shahriarh655@gmail.com>
2024-04-26 17:03:13 +0600

    Fixed a minor issue from feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,6 +3,7 @@ const Feature = ({ title, text }) => {
   return (
     <div className="features_container_feature">
       <div className="features_container_feature_title">
+        <div />
         <h1>{title}</h1>
       </div>
       <div className="features_container_feature_text">{text}</div>
