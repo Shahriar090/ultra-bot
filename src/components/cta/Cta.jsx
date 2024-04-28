06f07b95811b33bb9ba6dd5db272fbd63d3eec1e
Shahriar <shahriarh655@gmail.com>
2024-04-28 21:50:06 +0600

    Implemented Click To Action (CTA) component

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,6 +1,16 @@
 import "./cta.css";
 const Cta = () => {
-  return <div>cta</div>;
+  return (
+    <div className="cta">
+      <div className="cta_content">
+        <p>Request Early Access To Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="cta_button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
